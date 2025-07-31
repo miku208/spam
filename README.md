c673bc7f27e19b4cd864e00f2353a5ad5f75cf47
miku208 <suronos907@gmail.com>
2025-07-31 11:07:09 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# spam
+# Spam email
+---
+## cara install 
+```bash
+pkg update && pkg upgrade -y
+pkg install nodejs -y
+pkg install git -y
+git clone https://github.com/miku208/spam
+cd spam
+npm install
+npm start
+```
+--- 
+## 𝙅𝙄𝙆𝘼 𝙀𝙍𝙍𝙊𝙍 𝘽𝙀𝙍𝘼𝙍𝙏𝙄 𝙇𝙄𝙈𝙄𝙏 𝘿𝘼𝙉 𝙅𝙄𝙆𝘼 𝙇𝙄𝙈𝙄𝙏 𝙏𝙐𝙉𝙂𝙂𝙐 𝙎𝘼𝙈𝙋𝙀 24 𝙅𝘼𝙈
---
+++ config.js
@@ -0,0 +1,30 @@
+/*pakai akun email sendiri
+kalauau lebih dari 2 bisa tinggal hilangin blok komentar itu*/
+/*https://accounts.google.com/v3/signin/challenge/pk/presend?TL=ALgCv6zJwqdO8yez0vzyX0gg1nub-CP8IwUGKVGgF9RGFaSDB1xVdRc_Ms9mMFuB&authuser=1&cid=1&continue=https%3A%2F%2Fmyaccount.google.com%2Fu%2F1%2Fapppasswords%3Frapt%3DAEjHL4MCqaLVZev8wvVgZ1ntMfF3aa7Mo6CkE02VmRsqffgHiYLY4A6NEuyQRml0lsZ6Eaxw9pKUpfjsY_Ob8IzcKUNzYqxmO5oJTSezBwpyodWFxoUupKY&flowName=GlifWebSignIn&followup=https%3A%2F%2Fmyaccount.google.com%2Fu%2F1%2Fapppasswords%3Frapt%3DAEjHL4MCqaLVZev8wvVgZ1ntMfF3aa7Mo6CkE02VmRsqffgHiYLY4A6NEuyQRml0lsZ6Eaxw9pKUpfjsY_Ob8IzcKUNzYqxmO5oJTSezBwpyodWFxoUupKY&ifkv=AdBytiNdruD9LWwTe9a_DblgheM-SQhJqj9G9s5XsV2VwKUq7kspfNhZbvXIytl28-R1Rz5ZdMtt&osid=1&rart=ANgoxceiSy96ELUTXn57US79jIEMpsp1N1zGM35E5zsTYuLg7ODvv2aMCoc-PuVGwEyKPEMG6t5nP69ti8yAh9mF75895QTlCsDJkAcSKbF_PV7M_VRWNbI&rpbg=1&service=accountsettings */
+
+/* buka link di atas untuk buat pasword nya patikan 2fa aktif ya 
+jangan lupa di browser aktifkan mode dekstop*/
+module.exports = {
+  accounts: [
+    {
+      user: "akun1@gmail.com",
+      pass: "app_password_akun1",
+    },
+    {
+      user: "gangguanjawa09@gmail.com",
+      pass: "iphq otyd weur xcte",
+    },
+ /*   {
+      user: "akun3@gmail.com",
+      pass: "app_password_akun3",
+    },
+    {
+      user: "akun4@gmail.com",
+      pass: "app_password_akun4",
+    },
+    {
+      user: "akun5@gmail.com",
+      pass: "app_password_akun5",
+    },*/
+  ],
+};
